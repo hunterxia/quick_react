a718f4db7a7a8740831d78aa88cce59db8931a03
Hunterxia <hunterxia@outlook.com>
2023-10-11 18:42:36 -0500

    Add database

---
+++ src/utilities/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKvAUIGiB_zTX8gn5tKM7cIS9eHChm2io",
+  authDomain: "quick-react-4dc21.firebaseapp.com",
+  databaseURL: "https://quick-react-4dc21-default-rtdb.firebaseio.com",
+  projectId: "quick-react-4dc21",
+  storageBucket: "quick-react-4dc21.appspot.com",
+  messagingSenderId: "530009557608",
+  appId: "1:530009557608:web:124e8ecad414de17a649dd",
+  measurementId: "G-WB0FTKK1MB",
+};
+
+const firebase = initializeApp(firebaseConfig);
